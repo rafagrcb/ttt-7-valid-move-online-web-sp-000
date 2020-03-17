1ba42a6dd2c4b53d40aa74ace68a283fd28574cb
Rafael Garcia <rafagrcb@gmail.com>
2020-03-17 20:55:11 +0000

    Automatically backed up by Learn

--- lib/valid_move.rb
+++ lib/valid_move.rb
@@ -1,6 +1,7 @@
 # code your #valid_move? method here
 
-def valid_move()
+def valid_move?()
+  if valid_move(0,8)
 
 
 # re-define your #position_taken? method here, so that you can use it in the #valid_move? method above.
